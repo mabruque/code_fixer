cfe38799f0050ad2f149a970b0e13a338a5b1381
mabruk <mabruk@m3.mint>
2025-06-25 23:41:15 +0530

    write_file

---
+++ functions/write_file.py
@@ -0,0 +1,18 @@
+import os
+
+
+def write_file(working_directory, file_path, content):
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+        if not abs_file_path.startswith(abs_working_dir):
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+        return (
+            f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        )
+    except Exception as error:
+        return f"Error: {str(error)}"
--- tests.py
+++ tests.py
@@ -1,5 +1,4 @@
-from functions.get_file_content import get_file_content
-
+from functions.write_file import write_file
 
 # result = get_files_info("calculator", ".")
 # print("Result for current directory")
@@ -18,14 +17,27 @@ from functions.get_file_content import get_file_content
 # print(result)
 
 
-result = get_file_content("calculator", "main.py")
-print("Result for main.py in calculator")
+# result = get_file_content("calculator", "main.py")
+# print("Result for main.py in calculator")
+# print(result)
+
+# result = get_file_content("calculator", "pkg/calculator.py")
+# print("Result for pkg/calculator.py in calculator")
+# print(result)
+
+# result = get_file_content("calculator", "/bin/cat")
+# print("Result for /bin/cat in calculator")
+# print(result)
+
+
+result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+print("Writing lorem.txt in calculator")
 print(result)
 
-result = get_file_content("calculator", "pkg/calculator.py")
-print("Result for pkg/calculator.py in calculator")
+result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+print("Writing pkg/morelorem.txt in calculator")
 print(result)
 
-result = get_file_content("calculator", "/bin/cat")
-print("Result for /bin/cat in calculator")
+result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+print("Writing /tmp/temp.txt in calculator")
 print(result)
